5880e94bd8767601683f2a9f2017d1e216660ba6
mirazbd123 <mirazalambd1230@gmail.com>
2025-07-13 12:00:37 +0600

    Day01

--- Day-1/app.js
+++ Day-1/app.js
@@ -1,1 +1,83 @@
-console.loag("hellow Work")
+// variables 
+let name = 'Miraz' , age = 26 , position = 'DevOps';
+console.log(name);
+console.log(age);
+console.log(position);
+// assigning new values
+name = 'Miraz Khan';
+age = 25;
+position = 'DevOps Engineer';
+console.log(name);
+console.log(age);
+console.log(position);
+
+// constants
+const name1 = 'Miraz' , age1 = 26 , position1 = 'DevOps';
+console.log(name1);
+console.log(age1);
+console.log(position1);
+
+// // can assign new values --> not possible
+// name1 = 'Miraz Khan';
+// age1 = 25.5;
+// position1 = 'DevOps Engineer II';
+// console.log(name1);
+// console.log(age1);
+// console.log(position1);
+
+// Test
+let test1 = 2 + 3 + "5"; 
+console.log(test1); // output 55
+// datatype of test1
+console.log(typeof(test1)); // string
+// datatype of 2 + 3 + "5"
+console.log(typeof(2 + 3 + "5")); // string
+
+// Test
+let $ = "Hello World";
+let $$$ = 2;
+let $myMoney = 5;
+console.log($);
+console.log($$$);
+console.log($myMoney);
+
+// Test
+let _lastName = "Johnson";
+let _x = 2;
+let _100 = 5;
+console.log(_lastName);
+console.log(_x);
+console.log(_100);
+
+// Block scope 
+{
+    let name = 'Soboz'; // if i use let its the local scope
+    console.log(name); // soboz
+}
+console.log(name); // miraz khan
+
+{
+    var glob = 'Global'; // if i use var its the global scope
+    console.log(glob); // global
+}
+console.log(glob); // global
+
+// // Test
+// let x = "John Doe";
+
+// let x = 0;
+
+// console.log(x);
+// using let it will throw an error
+
+// useing var it wont
+var x = "John Doe";
+console.log(x);
+var x = 0;
+console.log(x);
+
+// cannot reassigned const variable
+const carName = "lolex";
+console.log(carName);
+// carName = "BMW"; // will throw an error
+// console.log(carName);
